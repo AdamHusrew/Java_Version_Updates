5b6e389f60189ee9d38038b9321e4d4c41f246f5
Adam Husrew <Adam.Husrew@cydeo.com>
2022-10-26 12:07:28 +0300

    DAy 43 Enum Completed

---
+++ Enum/src/main/java/org/cydeo/Operation.java
@@ -0,0 +1,9 @@
+package org.cydeo;
+
+public enum Operation {
+    PLUS,MINUS,DIVIDE,MULTIPLY;
+
+    Operation() {
+        System.out.println("Constructor");
+    }
+}
---
+++ Enum/src/main/java/org/cydeo/ProjectStatus.java
@@ -0,0 +1,14 @@
+package org.cydeo;
+
+public enum ProjectStatus {
+    OPEN,CLOSED,PROGRESS("IN PROGRESS");
+
+    String status;
+
+    ProjectStatus() {
+    }
+
+    ProjectStatus(String status) {
+        this.status = status;
+    }
+}
